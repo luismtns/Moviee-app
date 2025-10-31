e19e765ceff962ae80e1752515b5615c335b442f
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-10-31 13:51:52 -0300

    refactor: Remove Loading, PopularMoviesDemo, and VirtualizedMovieList components along with associated styles

--- src/components/VirtualizedMovieList/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as VirtualizedMovieGrid } from './VirtualizedMovieGrid'
-export { default as VirtualizedMovieList } from './VirtualizedMovieList'
-export { default as VirtualizedMovieListHorizontal } from './VirtualizedMovieListHorizontal'
