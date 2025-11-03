c9051f7b80c31b1fbc74067e7cb20a016b37f28f
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-11-03 03:08:05 -0300

    feat: Refactor FavoritesPage to streamline movie data retrieval and loading logic

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,28 +1,42 @@
 import Header from '@/components/Header/Header'
 import VirtualizedMovieGrid from '@/components/VirtualizedMovieGrid/VirtualizedMovieGrid'
-import { useFavorites } from '@/hooks/useFavorites'
 import { useFavoriteMovies } from '@/hooks/useMovies'
 import { Movie } from '@/types/Movie'
 import { IonContent, IonPage } from '@ionic/react'
-import React from 'react'
+import React, { useMemo } from 'react'
 import './FavoritesPage.css'
 
 const FavoritesPage: React.FC = () => {
-  const { favoriteIds } = useFavorites()
-  const { data: detailedMovies } = useFavoriteMovies(favoriteIds)
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } = useFavoriteMovies()
 
-  const allMovies = detailedMovies?.map((movie) => ({
-    id: movie.id,
-    title: movie.title,
-    poster_path: movie.poster_path,
-    release_date: movie.release_date,
-    vote_average: movie.vote_average,
-  }))
+  const allMovies = useMemo(() => {
+    if (!data?.pages) return []
+
+    return data.pages.flatMap((page) =>
+      page.results.map(
+        (movie) =>
+          ({
+            id: movie.id,
+            title: movie.title,
+            poster_path: movie.poster_path,
+            release_date: movie.release_date,
+            vote_average: movie.vote_average,
+          } as Movie)
+      )
+    )
+  }, [data])
+
+  const handleLoadMore = () => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage()
+    }
+  }
+
   return (
     <IonPage>
       <Header />
       <IonContent scrollY={false}>
-        <VirtualizedMovieGrid movies={allMovies?.map((movie) => movie as Movie) || []} />
+        <VirtualizedMovieGrid movies={allMovies} onLoadMore={handleLoadMore} isFetchingNextPage={isFetchingNextPage} />
       </IonContent>
     </IonPage>
   )
