6b712ae7cfb727817b75651483caeba486954ab2
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-11-03 17:58:11 -0300

    refactor: Simplify Home component by removing search functionality and related logic

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,52 +1,24 @@
 import Header from '@/components/Header/Header'
 import VirtualizedMovieGrid from '@/components/VirtualizedMovieGrid/VirtualizedMovieGrid'
-import { usePopularMovies, useSearchMovies } from '@/hooks/useMovies'
-import { useSearchStore } from '@/stores/searchStore'
+import { usePopularMovies } from '@/hooks/useMovies'
 import { IonContent, IonPage } from '@ionic/react'
 import { useMemo } from 'react'
 import './Home.css'
 
 const Home: React.FC = () => {
-  const { searchTerm } = useSearchStore()
+  const { data: popularData, fetchNextPage, isPending, isFetchingNextPage } = usePopularMovies()
 
-  const {
-    data: popularData,
-    fetchNextPage: fetchNextPopular,
-    isLoading: isLoadingPopular,
-    isFetchingNextPage: isFetchingNextPagePopular,
-  } = usePopularMovies()
-
-  const {
-    data: searchData,
-    fetchNextPage: fetchNextSearch,
-    isLoading: isLoadingSearch,
-    isFetchingNextPage: isFetchingNextPageSearch,
-  } = useSearchMovies(searchTerm, searchTerm.length > 2)
-
-  const isSearching = searchTerm.length > 2
-  const displayData = isSearching ? searchData : popularData
-  const isLoading = isSearching ? isLoadingSearch : isLoadingPopular
-  const isFetchingNextPage = isSearching ? isFetchingNextPageSearch : isFetchingNextPagePopular
-
-  const allMovies = useMemo(() => displayData?.pages.flatMap((page) => page.results) || [], [displayData])
-
-  const handleLoadMore = () => {
-    if (isSearching) {
-      fetchNextSearch()
-    } else {
-      fetchNextPopular()
-    }
-  }
+  const allMovies = useMemo(() => popularData?.pages.flatMap((page) => page.results) || [], [popularData])
 
   return (
     <IonPage>
       <Header />
-      {/* https://ionicframework.com/docs/react/virtual-scroll */}
       <IonContent scrollY={false}>
         <VirtualizedMovieGrid
           movies={allMovies}
-          onLoadMore={handleLoadMore}
-          isFetchingNextPage={isFetchingNextPage || isLoading}
+          isPending={isPending}
+          onLoadMore={fetchNextPage}
+          isFetchingNextPage={isFetchingNextPage || isPending}
         />
       </IonContent>
     </IonPage>
