47770a0344b1ba30ab57a4c3d001e1b6813645f7
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-11-03 03:32:27 -0300

    test: Update Logo component test to check for ion-img with correct alt attribute

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Logo from './Logo'
 
 describe('Logo', () => {
-  it('renders logo image', () => {
-    render(<Logo />)
+  it('renders ion-img with correct alt', () => {
+    const { container } = render(<Logo />)
 
-    const logo = screen.getByAltText('Moviee Logo')
-    expect(logo).toBeTruthy()
+    const logo = container.querySelector('ion-img')
+    expect(logo?.getAttribute('alt')).toBe('Moviee Logo')
   })
 })
