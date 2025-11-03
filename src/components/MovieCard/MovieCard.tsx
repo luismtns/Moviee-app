7d0aeb91d74809817656fce39a8889e52b4adb08
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-11-03 05:59:03 -0300

    style: Update MovieCard CSS variables and structure for improved layout

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,25 +1,33 @@
 import type { Movie } from '@/types/Movie'
 import { IonCard } from '@ionic/react'
 import { memo } from 'react'
+import { useHistory } from 'react-router-dom'
 import './MovieCard.css'
 import { MovieActions, MovieFavoriteButton, MovieImage, MovieInfo, MovieRating } from './index'
 
 interface MovieCardProps {
   movie: Movie
-  // onFavoriteToggle?: (movieId: number, title: string, isFavorite: boolean) => void
 }
 
 const MovieCard: React.FC<MovieCardProps> = memo(({ movie }) => {
+  const history = useHistory()
+
+  const handleCardClick = () => {
+    history.push(`/movie/${movie.id}`)
+  }
+
   return (
-    <IonCard className='movie-card'>
-      <MovieImage posterPath={movie.poster_path} title={movie.title} />
+    <IonCard className='movie-card' button onClick={handleCardClick}>
+      <div className='movie-card-wrapper'>
+        <MovieImage posterPath={movie.poster_path} title={movie.title} />
 
-      <MovieInfo title={movie.title} releaseDate={movie.release_date} />
+        <MovieInfo title={movie.title} releaseDate={movie.release_date} />
 
-      <MovieActions>
-        <MovieRating rating={movie.vote_average} />
-        <MovieFavoriteButton movieId={movie.id} movieTitle={movie.title} />
-      </MovieActions>
+        <MovieActions>
+          <MovieRating rating={movie.vote_average} />
+          <MovieFavoriteButton movieId={movie.id} movieTitle={movie.title} />
+        </MovieActions>
+      </div>
     </IonCard>
   )
 })
