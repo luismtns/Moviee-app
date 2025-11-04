b177a6dcd255437c979f1e31f3ec498f8233be45
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-11-04 00:08:58 -0300

    Update Logo component styles and image source
    - Increased logo dimensions in Logo.css for better visibility.
    - Changed the logo image source in Logo.tsx to use a new logotypo image.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import { IonImg } from '@ionic/react'
 import './Logo.css'
 
 function Logo() {
-  return <IonImg src='/assets/moviee-logo.svg' className='logo-moviee' alt='Moviee Logo' />
+  return <IonImg src='/assets/moviee-logotypo.svg' aria-label='Moviee' className='logo-moviee' alt='Moviee Logo' />
 }
 
 export default Logo
