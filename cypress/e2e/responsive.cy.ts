7428a030c2e7800b51239f32845e0d1cecc3939c
Luís Bovo <31661255+luismtns@users.noreply.github.com>
2025-11-04 23:46:08 -0300

    refactor: remove desktop test case from responsive.cy.ts for streamlined tests

--- cypress/e2e/responsive.cy.ts
+++ cypress/e2e/responsive.cy.ts
@@ -9,14 +9,4 @@ describe('Responsive', () => {
     cy.get('ion-tab-bar').should('be.visible')
     cy.get('ion-card').should('exist')
   })
-
-  it('displays correctly on desktop', () => {
-    cy.intercept('GET', '**/movie/popular*', { fixture: 'movies.json' })
-    cy.viewport(1280, 720)
-    cy.visit('/')
-    cy.waitForIonic()
-
-    cy.get('ion-app').should('be.visible')
-    cy.get('ion-card').should('exist')
-  })
 })
